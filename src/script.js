f5f139758563076ac950efca4f2f299aaeb68fe3
vamsiinnocent <innocentvamsi2@gmail.com>
2025-01-06 11:38:21 +0000

    separated out the script file

---
+++ src/script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const display = document.getElementById('calc-display');
+    const buttons = document.getElementsByClassName('btn'); 
+
+    let currentValue = "";
+
+    function evaluateResult() {
+     console.log('currentValue:', currentValue)
+     const convertedValue = currentValue
+     .replace("×", "*" )
+     .replace("+", "/")
+     .replace('%', '*0.01');
+     console.log('convertedValue:', convertedValue)
+     const result = eval(convertedValue);
+     currentValue = result.toString();
+     display.value = currentValue;
+    }
+     
+    for (let i = 0; i < buttons.length; i++) {
+        const button = button[i];
+        button.addEventListener('click', function() {
+        const value = button.innerText;
+
+        if (value == "AC") {
+            currentValue = "";
+            display.value = currentValue;
+        } else if (value == "="){
+          evaluateResult();
+        } else { 
+            currentValue += value;
+            display.value = currentValue;
+        }
+
+        
+        })
+    }
+
+
+  });
